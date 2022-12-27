4f067786cf608646b755e0f7d97dbf1a586fe46a
kyoungholee <dlrudgh1303@gmail.com>
2022-12-27 13:43:44 +0900

    [생성] 환경설정 구성

---
+++ server/index.js
@@ -0,0 +1,12 @@
+
+
+
+const liveServer = require("live-server");
+
+const params = {
+	port: 3000, // Set the server port. Defaults to 8080.
+	host: 'localhost', // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+	root: "./client", // Set root directory that's being served. Defaults to cwd.
+	open: false, // When false, it won't load your browser by default.
+};
+liveServer.start(params);
