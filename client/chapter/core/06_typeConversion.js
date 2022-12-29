1ced464e221b9076f2ce4a9a9ab6365b6b703400
kyoungholee <dlrudgh1303@gmail.com>
2022-12-29 11:19:28 +0900

    비교 연산자 학습

--- client/chapter/core/06_typeConversion.js
+++ client/chapter/core/06_typeConversion.js
@@ -7,25 +7,25 @@
 
 // number
 
-const year =2022;
-console.log(year);
+// const year =2022;
+// console.log(year);
 
 
-// undefined, null
+// // undefined, null
 
-console.log( (undefined) );
+// console.log( (undefined) );
 
-// boolean
+// // boolean
 
-let kind = true;
-console.log(kind);
+// let kind = true;
+// console.log(kind);
 
 
 /* 데이터 → 숫자 ----------------------------------------------------------- */
 
 // undeinfed
 
-console.log(Number(undefined) );
+// console.log(Number(undefined) );
 
 
 // null
@@ -40,7 +40,7 @@ console.log(Number(undefined) );
 
 let width = "320px";
 
-console.log(parseInt(width, 10));
+// console.log(parseInt(width, 10));
 
 
 /* 데이터 → 불리언 ---------------------------------------------------------- */
@@ -49,4 +49,24 @@ console.log(parseInt(width, 10));
 // 위에 나열한 것 이외의 것들 
 
 
-console.log(!!null);
+var a = "100";
+
+function outer() {
+  var b = "10";
+  console.log(a); //100 
+  console.log(b); // 10
+
+  function inner() {
+    var a = "14";
+
+    console.log(a); //14
+    console.log(b); //10
+
+  }
+  inner();
+
+}
+
+outer();
+console.log(a); //10
+console.log(b); //레버런스 에러~ 
