aca674ae3e538c18d9d9d361c608148475fb1176
kyoungholee <dlrudgh1303@gmail.com>
2023-01-13 16:02:44 +0900

    [학습] 주접 생성기 학습 !!

--- client/main.js
+++ client/main.js
@@ -1,2 +1,49 @@
+import {getNode} from './lib/dom/getNode.js';
+import {jujeobData} from './data/data.js';
+import { getRandom } from './lib/math/getRandom.js';
+import { isNumericString } from './lib/utils/typeOf.js';
+import { showAlert } from './lib/dom/showAlert.js';
 
-console.log("hello")
+const submit = getNode('#submit');
+const nameField = getNode('#nameField');
+const result = getNode('.result');
+
+
+console.log ( isNumericString('as123') );
+
+
+function clickSubmitHandler(e) {
+  e.preventDefault();
+  let name = nameField.value;
+
+  //리턴 값이 배열이니깐 !! 
+  let list = jujeobData(name);
+  //console.log(list);
+
+  if(!name) {
+  
+    showAlert('.alert', '올바른 정보를 입력해주세요', 3000);
+  }
+  if(isNumericString(name)) {
+    console.log("숫자야!");
+    showAlert('.alert', "에러입니다", 2000);
+  }
+
+  let pick = list[getRandom(list.length-1)];
+  console.log(pick);
+
+  // console.log(list);
+
+  // console.log( name);
+
+  //화면에 보여주는 역할!!!~~ 화면에 보여주자 ~ 
+
+
+  result.textContent="";
+  result.insertAdjacentHTML('afterbegin', pick);
+
+
+
+}
+
+submit.addEventListener('click', clickSubmitHandler);
