0d1c0d4133a5a1cde4daecddeb1358cf3bdecc84
kyoungholee <dlrudgh1303@gmail.com>
2023-01-16 22:23:57 +0900

    [학습] 랜덤 다이스 주석 추가

--- client/lib/utils/memo.js
+++ client/lib/utils/memo.js
@@ -1,10 +1,7 @@
-
-
-/** 메모제이션을 이용한 클로저 예제 문제!!!!!!!!!!! 
+/** 메모제이션을 이용한 클로저 예제 문제!!
  * 메모제이션에 callback 함수를 기억하고 
  * 다시 key 값만 불러오게 되면 기억되어있는 공간에서 value을 반환합나다.
  */
-
 
 /**react에서의 useMemo와 비슷한 개념이다 !!!! 
  * 
@@ -17,43 +14,33 @@ export const memo = () => {
   
   const cache = {}
 
+  //클로저로 만들기 위함
   return (key, callback) => {
 
       //get 부분 단순히 보여준다. 
   if(!callback) { 
-    console.log("반환")
     return cache[key]; //cache 키의 반환 값 
   }
-
-
 
   if(cache[key]) { //cache에 키가 이미 있어?? 그럼 그냥 return 만해 !!
     console.warn("이미 키 값이 존재 합니다.")
     return;
   }
 
-            //콜백함수 실행      콜백함수로 굳이 매개변수로 받는 이유는 많은 양에 코드를 넣기 위함 
-            cache[key] = callback(); //key에 value 값으로 들어감 
-          //여기서 callback 함수가 기록 됩니다. 
-          //받은 콜백함수를 호출한 다음 cache[key] 값에 함수 호출 값을 저장해준다.
+   //콜백함수 실행  
+  cache[key] = callback(); //key에 value 값으로 들어감 
+  //여기서 callback 함수가 기록 됩니다. 
+  //받은 콜백함수를 호출한 다음 cache[key] 값에 함수 호출 값을 저장해준다.
 
-            console.log(cache);
-          
+  console.log(cache);        
   }
 }
 
-
-
-
-
-
-
 //값을 설정한다 set
-
 //클로저 실행시 !!! 한번 더 호출을 해줘야한다.
-memo()('cube', ()=> document.querySelector('#cube'));
+//이런식으로  자주 사용되는 node를  cache 객체 안에 기록하면 좋습니다. 
+memo()('cube', ()=> document.querySelector('#cube')); // 
 
-
-//위에서 설정한 값을 가져옴  get!!! 
-//클로저 실행시 !!! 한번 더 호출을 해줘야한다.
+//위에서 설정한 값을 가져옴  get!
+//클로저 실행시 ! 한번 더 호출을 해줘야한다.
 memo()('cube');
--- client/main.js
+++ client/main.js
@@ -1,37 +1,27 @@
 import { diceAnimation, insertLast, invisibleElement, visibleElement ,  attr, clearContents} from "./lib/index.js";
 import { getNode ,getNodes} from "./lib/dom/getNode.js";
 
-// [ 레코드 리스트 보이기 ]
-// 1. handleRecord 함수를 만들기
-// 2. disable 활성 유틸 함수 만들기
-// 3. handleReset 함수를 만듭니다.
-// 4. visible 활성 유틸 함수 만들기
-// 5. toggleState 유틸 함수 만들기 
-
 // class buttonGroup안에 모든 버튼 태그를 전부 가져온다.
 //배열에 구조분해할당---> 밑에 처럼 자식요소들을 하나씩 변수로 받을 필요가 없다.
 //구조분해 할당으로 변수들을 받으면 알아서 자리를 찾아간다.
 const [rollingDiceButton, recordButton, resetButton ] = getNodes('.buttonGroup > button');
-const recordListWrapper = getNode('.recordListWrapper');
 
 //const rollingDiceButton = getNode('.buttonGroup > button:ntn-child(1)');
-
-//HTML에 있는 두번째 속성을 가지고 오는 방법 (버튼 태그의) type에 두번째 자식의 속성을 가져옴
+//HTML에 .buttonGroup에 있는 두번째 자식 속성을 가지고 오는 방법 (버튼 태그의) type에 두번째 자식의 속성을 가져옴
 //const recordButton = getNode('.buttonGroup > button:ntn-child(2)');
 //const resetButton = getNode('.buttonGroup > button:ntn-child(3)');
 
+const recordListWrapper = getNode('.recordListWrapper');
 const numberRecord = getNode('#cube');
-
 const tdValue = getNode('tbody');
 
 let count = 1;
 let result = 0;
+
 function renderRecordListItem() {
 
-
-
   //주사위에 무작위 나온 값을 넣어줘라
-  // 값을가져오는 것  
+  // 값을가져오는 것  getAttribute!! 한 모습
   let recodeNumber = Number( attr('#cube', 'data-dice'));
 
   let result =+ recodeNumber;
@@ -43,29 +33,17 @@ function renderRecordListItem() {
     <td>${result}</td>
   </tr>`;
 
-  //useState안에는 클로저로 이루어져 있다. 
+  //useState안에는 클로저로 이루어져 있습니다. 
 
-  //뿌려주고 있다.  넣어서 보여줄 수 있다.
+  //뿌려주고 있습니다.  넣어서 보여줄 수 있습니다.
   insertLast('.recordListWrapper tbody', template);
   count++;
 
-  //코딩 하지 않을 경우 맨처음을 보여준다. 스크롤은 계속해서 내려가는데 
-  
-  
-  //스크롤 길이만큼 항목을 찍어준다.  하지만 이 코드를 적어주면 스크롤이 내려가는 만큼 가장 밑을 볼 수 있다.
-
+  //이부분을 코딩 하지 않을 경우 스크롤은 계속 내려가지만 가장 맨위에만 보여준다.
+  //스크롤 길이만큼 항목을 찍어준다.  
+  //하지만 이 코드를 적어주면 스크롤이 내려가는 만큼 가장 밑을 볼 수 있다.
   recordListWrapper.scrollTop = recordListWrapper.scrollHeight;
 }
-
-
-/**이벤트 구문
- * 
- * 
- * 
- * 
- */
-
-
 const handlerRollingDice = () => {
 
   let stopAnimation;
@@ -98,7 +76,8 @@ const handlerRollingDice = () => {
   }
 
   const handleRecord = () => {
-    visibleElement(recordListWrapper);
+    //visibleElement(recordListWrapper);
+    recordListWrapper.hidden = false;
     renderRecordListItem();
   }
 
@@ -106,20 +85,17 @@ const handlerRollingDice = () => {
     count = 0;
     result = 0;
     
-    invisibleElement(recordListWrapper);
+    // invisibleElement(recordListWrapper);
+    recordListWrapper.hidden = true;
     clearContents('.recordListWrapper tbody');
-
     //('.recordListWrapper tbody').textContext = "";
   }
-
-
-  //handlerRollingDice()()
+  
                                               //이벤트 발생시 함수자체를 반환해줍니다.
   //위에서 클로저를 사용할 경우 return문에 있는 함수를  실행해줘야 하기 때문에 handlerRollingDice함수에
   // ()를 추가해줘야한다.
                                       //이벤트핸들러에서 콜백함수가 호출 되고 있다면 거의 클로저입니다.
 rollingDiceButton.addEventListener('click', handlerRollingDice());
-
 
 recordButton.addEventListener('click', handleRecord);
 
