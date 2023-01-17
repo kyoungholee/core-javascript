d070c8709de6ce9d5cbc4bd8df16df548d89bcc7
kyoungholee <dlrudgh1303@gmail.com>
2023-01-17 09:14:14 +0900

    [연습] 비동기 통신

--- client/main.js
+++ client/main.js
@@ -1,2 +1,17 @@
+const pr = new Promise ((resolve, reject) => {
+  console.log("내가 첫번쨰따!!!!!!!!!!!!!!!!!") // 이부부은 비동기임 !! 
+  setTimeout( ()=> {
+    resolve("OK");
+  }, 1000);
+})
 
-console.log("hello")
+
+console.log("asdasd");
+
+pr.then( (내가받음) => {
+  console.log(내가받음);
+})
+
+.catch((err) => {
+  console.log(err);
+})
